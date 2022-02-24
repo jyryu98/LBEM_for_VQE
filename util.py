b20ea6394b9bc0b4cbea9702f1fea8e109cb5104
Eyuel Eshetu <54147950+Eyuel-E@users.noreply.github.com>
2022-02-25 00:21:39 +0900

    Update util.py

--- util.py
+++ util.py
@@ -142,7 +142,7 @@ def grouping(final_solution, obs_hamiltonian ):
         grouped_list.append(temp)
     return grouped_list
 
-
+  
 def get_ansatz(n,m,ansatz = 'simple'):
     ## trivial circuit for H2 check might not work properly
     phi = Parameter('phi')
@@ -152,10 +152,23 @@ def get_ansatz(n,m,ansatz = 'simple'):
         qc.cx(0,1)
         qc.rx(phi,0)
         qc.cx(1,0)
+        num_par_gate = 1
     elif ansatz == 'num_particle_preserving':
-        qc = n_qubit_A_circuit(n,m)
+        qc, num_par_gate = n_qubit_A_circuit(n,m)
     else:
         raise NotImplementedError
+    return qc, num_par_gate
+
+
+def get_measuring_circuit(basis: str) -> QuantumCircuit:
+    qc = QuantumCircuit(len(basis))
+    for i,term in enumerate(basis):
+        if term == 'X':
+            qc.h(i)
+        elif term == 'Y':
+            qc.rx(math.pi/2,i)
+        else:
+            continue
     return qc
 
 
@@ -180,8 +193,10 @@ def n_qubit_A_circuit(n,m, repeat = 1):
                 A_gate(qc,i,j,theta[index])
                 index += 1
     
-    return qc
+    return qc, index
         
+
+
 
 
 def main():
@@ -190,11 +205,11 @@ def main():
     distance = 1.4
     qubit_ham = calculate_pauli_hamiltonian(molecule_name, distance)['qubit_operator']
     optimized = optimize_measurements( [list(term[1]) for term in qubit_ham] )
-    
     group_pauli_op = grouping(optimized,qubit_ham)
-    ansatz = get_ansatz(6,2,'num_particle_preserving')
+    ansatz, num_par_gates = get_ansatz(6,2,'num_particle_preserving')
+
+    print(len(group_pauli_op))
 
 
-
-    return group_pauli_op, ansatz
+    return group_pauli_op, [ansatz,num_par_gates]
 
