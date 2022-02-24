c2a55e64585f8513b9103712ea725d2ec2526868
Ju-Young Ryu <72022558+jyryu98@users.noreply.github.com>
2022-02-24 23:47:08 +0900

    Update util.py

--- util.py
+++ util.py
@@ -14,19 +14,6 @@ def get_ansatz(n,m,ansatz = 'simple'):
         raise NotImplementedError
     return qc, num_par_gate
 
-
-def get_measuring_circuit(basis: str) -> QuantumCircuit:
-    qc = QuantumCircuit(len(basis))
-    for i,term in enumerate(basis):
-        if term == 'X':
-            qc.h(i)
-        elif term == 'Y':
-            qc.rx(math.pi/2,i)
-        else:
-            continue
-    return qc
-
-
 def A_gate(qc, qubit1 , qubit2, theta):
     qc.cx(qubit2,qubit1)
     qc.ry(theta + math.pi/2,qubit2)
